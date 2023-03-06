3384a1ed970619a5733b9be5c282c39f3428c473
Asjad MN1 Ali <EXPASAL@sallinggroup.com>
2023-03-06 18:19:19 +0500

    End of secftion 3

---
+++ Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,12 @@
+using API.Entities;
+
+namespace Core.Interfaces
+{
+    public interface IProductRepository
+    {
+     Task<Product> GetProductByIdAsync(int Id);
+     Task<IReadOnlyList<Product>> GetProductsAsync();   
+     Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync();  
+     Task<IReadOnlyList<ProductType>> GetProductTypesAsync();  
+    }
+}
